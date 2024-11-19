b855d7614f0958a1aa6c00b9157c97f284f8bac2
fauzanmfzn <mhdfzn11@gmail.com>
2024-11-19 21:32:45 +0700

    fixing button

--- percobaan/masukkan.js
+++ percobaan/masukkan.js
@@ -4,6 +4,5 @@ const valueFile = document.getElementById("formFile");
 function clickButton(){
     if (valueSaran.value && valueFile.value) {
         window.location.href = "kitacoba.html";
-        console.log("klik button");
     } 
 }
